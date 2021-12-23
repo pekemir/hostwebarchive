6114d4bc8d12997dba845c0883b69cbee4ca55aa
pekemir <91409576+pekemir@users.noreply.github.com>
2021-12-23 06:58:00 +0100

    Update mainss.js

--- mainss.js
+++ mainss.js
@@ -6,7 +6,7 @@ var controlpagina=0;
 var cantidaddenoticias;
 const datal=JSON.stringify( datas);
 const noticias22 = JSON.parse(datal);
-const noticias;
+var noticias;
 var noticiasmostradas=4;
 const myH1=document.getElementById('h1primeranoticia');
       const myH2=document.getElementById('h1segundanoticia');
@@ -29,6 +29,7 @@ $(document).ready( function () {
     showNoticias(1);
      });
 function consultalistData(numero) {
+
       $.ajax({
         url: 'https://hockeysalduie78.es/pagina2.php',
         type: 'POST',
